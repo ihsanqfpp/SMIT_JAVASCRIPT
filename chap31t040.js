f7c82801f8694ef956ecd4e4a7c268dee2f51e42
ihsanqfpp <okzihsan956@gmail.com>
2024-09-20 22:38:47 +0500

    init

---
+++ chap31t040.js
@@ -0,0 +1,63 @@
+// 31  Getting the current date and time
+
+// let date = new Date("March 20, 2025")
+// console.log(date)
+// console.log(date.getDay())
+// console.log(date.getFullYear()) //for year start from zero
+// console.log(date.getHours())
+// console.log(date.getMonth())
+// console.log(date.getMinutes())
+// console.log(date.getTime())
+// console.log(date.getMilliseconds())
+// console.log(date.getTimezoneOffset())
+// console.log(date.getUTCDate())
+// console.log(date.getUTCDay())
+
+
+    //RETRIVE CURRENT DAY
+//     let  dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// let now = new Date();
+// let theDay = now.getDay();
+// let nameOfToday = dayNames[theDay];
+// console.log(nameOfToday)
+ 
+
+   //RETRIVE CURRENT MONTH
+// let month = ["january", "february","march","april","may","june","july","august","september",
+// "october","november","december"];
+// let now = new Date()
+// let currentMonth =now.getMonth()
+// let monthName = month[currentMonth]
+// console.log(monthName);
+
+// let d = new Date();
+// d.setMonth(11);
+// console.log(d)
+
+// let a = new Date()
+// a.setHours(10)
+// console.log(a)
+
+//35   Functions
+
+// function subtraction(num1 , num2){
+//     console.log(num1 - num2);
+// }
+// subtraction(10 , 5)
+// subtraction(100 , 50)
+
+// let time = new Date()
+
+// function counter(){
+//     console.log(time.getHours() +" : " + time.getMinutes())
+// }
+
+
+
+
+
+
+
+
+
+
