f8602febb3dcce0cf2bf868a5de730ac7bcc434d
ihsanqfpp <okzihsan956@gmail.com>
2024-10-18 09:58:15 +0500

    init

---
+++ prectise.js
@@ -0,0 +1,14 @@
+let country = "pakistan"
+let continent = "south asia"
+let population = 220000000
+
+//console.log("country name is : " +country  +" " +"and its continent is : " +continent +" " +"population is : " +population)
+//console.log(`country name is ${country} and its continent is ${continent} population is ${population}`);
+
+ let isiland = true;
+ let langauge;
+ console.log(typeof(country))
+ console.log(typeof(continent))
+ console.log(typeof(population))
+ console.log(typeof(isiland))
+ console.log(typeof(langauge))
